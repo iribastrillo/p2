0fa2b0bb1efd600cc0a8b90cda4659e85e34397f
Sintesis <iribastrillo@gmail.com>
2022-06-02 10:19:57 -0300

    ADD: agrega mvc al proyecto

---
+++ Dominio/User.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dominio
+{
+    abstract class User
+    {
+
+    }
+}
--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -23,7 +23,7 @@ namespace WebApp.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Team()
         {
             return View();
         }
