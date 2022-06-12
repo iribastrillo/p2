69cbe4cdf82549ff09b57e006be0f48ea10f9c35
Sintesis <iribastrillo@gmail.com>
2022-06-12 19:05:40 -0300

    STYLE: cambia estilos en el login con Bootstrap.

--- WebApp/Controllers/ClientController.cs
+++ WebApp/Controllers/ClientController.cs
@@ -3,11 +3,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Manager;
+
 
 namespace WebApp.Controllers
 {
     public class ClientController : Controller
     {
+        Manager.Manager instance = Manager.Manager.GetInstance();
         public IActionResult Likes ()
         {
             return View();
