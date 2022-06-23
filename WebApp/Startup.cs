bb91df872fa2fd2e9f3160935cd696faf7bb7400
Sintesis <iribastrillo@gmail.com>
2022-06-22 21:55:19 -0300

    STYLE: saca boludeces.

--- WebApp/Startup.cs
+++ WebApp/Startup.cs
@@ -21,18 +21,12 @@ namespace WebApp
 
 
         public IConfiguration Configuration { get; }
-
-
-
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
             services.AddSession();
         }
-
-
-
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
@@ -48,17 +42,9 @@ namespace WebApp
             }
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
-
-
             app.UseSession();
             app.UseRouting();
-
-
-
             app.UseAuthorization();
-
-
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
